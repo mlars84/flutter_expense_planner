c927b5e666d29371e5638e4283582197ae3ac96d
Matt Larson <matt.a.larson@gmail.com>
2021-01-07 15:40:07 -0600

    Small style upgrades

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -10,6 +10,15 @@ class NewTransaction extends StatelessWidget {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) return;
+
+    addNewTransaction(enteredTitle, enteredAmount);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -19,13 +28,16 @@ class NewTransaction extends StatelessWidget {
           TextField(
             decoration: InputDecoration(labelText: 'Title'),
             controller: titleController,
+            onSubmitted: (_) => submitData(),
           ),
           TextField(
             decoration: InputDecoration(labelText: 'Amount'),
             controller: amountController,
+            keyboardType: TextInputType.number,
+            onSubmitted: (_) => submitData(),
           ),
           FlatButton(
-            onPressed: () => addNewTransaction(titleController.text, double.parse(amountController.text)),
+            onPressed: () => submitData(),
             child: Text('Add Transation'),
             textColor: Colors.red,
           )
